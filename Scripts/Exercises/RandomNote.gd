ad1a90a6139b44607fc6f05639d9223c9bb0f0ce
rockrick1 <henriquecerquinho@gmail.com>
2026-02-16 14:34:56 -0300

    improve random note code

--- Scripts/Exercises/RandomNote.gd
+++ Scripts/Exercises/RandomNote.gd
@@ -16,16 +16,17 @@ func _delete_current_notes():
 
 # generates a random pitch value, different from the last
 func _spawn_random_note():
-	var pitch: int = last_note
+	var note := last_note
 	var note_str
-	if pitch > - 1:
-		note_str = NoteMapping.notes[pitch]
+	if note > - 1:
+		note_str = NoteMapping.notes[note]
 	# if accidentals are disabled, keep generating a new note until it doesn't have one
-	while pitch == last_note or ((not ExerciseController.accidentals) and ("#" in note_str or "b" in note_str)):
-		pitch = randi() % (pitch_range[1] - pitch_range[0]) + pitch_range[0]
-		note_str = NoteMapping.notes[pitch]
-	ExerciseController.add_note(pitch)
-	last_note = pitch
+	if !ExerciseController.accidentals:
+		while note == last_note or "#" in note_str or "b" in note_str:
+			note = randi() % (pitch_range[1] - pitch_range[0]) + pitch_range[0]
+			note_str = NoteMapping.notes[note]
+	ExerciseController.add_note(note)
+	last_note = note
 
 func next_step():
 	_spawn_random_note()
